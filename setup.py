95e5ce59563b384a4c0c00911905606a2f9731b9
zhiyuli <zyli2004@gmail.com>
2018-02-13 14:28:54 -0700

    add dependency into setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ app_class = 'my_brainishare_app.app:MyBrainishareApp'
 app_package_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tethysapp', app_package)
 
 ### Python Dependencies ###
-dependencies = []
+dependencies = ["requests"]
 
 setup(
     name=release_package,
